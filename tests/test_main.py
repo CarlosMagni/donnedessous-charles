19e7f2b3a7478fd068ff9bb79c3de9d994961cff
Charles Bournonville <carlosmagni@MacBook-Air-de-Charles.local>
2024-09-25 15:24:23 +0200

    fix add secret

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,7 +4,7 @@ from donnedessouscharles.main import donne_des_sous, compte_en_banque
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         compte_en_banque = donne_des_sous(8,10)
-        self.assertEquals(18, compte_en_banque)
+        self.assertEqual(18, compte_en_banque)
 
 if __name__ == '__main__':
     unittest.main()
